752d978e4ff2a335756b27b34e9e2b376eab426d
turncoat54 <392207009@qq.com>
2024-05-16 22:23:40 +0800

    [Picamera2] Add real time face recog on rpi
    - Add real time face recog code using picamera2 on raspberry pi 4B + wish aarch64 system
    
    Signed-off-by: turncoat54 <392207009@qq.com>

---
+++ examples/facerec_on_raspberry_pi_picamera2.py
@@ -0,0 +1,88 @@
+# This is a demo of running face recognition on a Raspberry Pi.
+# This program will print out the names of anyone it recognizes to the console.
+
+# To run this, you need a Raspberry Pi 2 (or greater) with face_recognition and
+# the picamera[array] module installed.
+# You can follow this installation instructions to get your RPi set up:
+# https://gist.github.com/ageitgey/1ac8dbe8572f3f533df6269dab35df65
+
+import face_recognition
+from picamera2 import Picamera2, Preview
+import cv2
+# import picamera
+import numpy as np
+
+# Get a reference to the Raspberry Pi camera.
+# If this fails, make sure you have a camera connected to the RPi and that you
+# enabled your camera in raspi-config and rebooted first.
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(main={"format": 'RGB888', "size": (320, 240)}))
+picam2.start()
+# output = np.empty((240, 320, 3), dtype=np.uint8)
+
+# Load a sample picture and learn how to recognize it.
+print("Loading known face image(s)")
+obama_image = face_recognition.load_image_file("obama_small.jpg")
+obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+# Create arrays of known face encodings and their names
+known_face_encodings = [
+    obama_face_encoding
+]
+known_face_names = [
+    "Barack Obama"
+]
+
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+
+while True:
+    print("Capturing image.")
+    # Grab a single frame of video from the RPi camera as a numpy array
+    # camera.capture(output, format="rgb")
+    # import pdb; pdb.set_trace()
+    frame = picam2.capture_array()
+
+    # Find all the faces and face encodings in the current frame of video
+    face_locations = face_recognition.face_locations(frame)
+    print("Found {} faces in image.".format(len(face_locations)))
+    face_encodings = face_recognition.face_encodings(frame, face_locations)
+
+    # Loop over each face found in the frame to see if it's someone we know.
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        name = "<Unknown Person>"
+
+
+
+        # If a match was found in known_face_encodings, just use the first one.
+        # if True in matches:
+        #     first_match_index = matches.index(True)
+        #     name = known_face_names[first_match_index]
+
+        # Or instead, use the known face with the smallest distance to the new face
+        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 10), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+
+        # print("I see someone named {}!".format(name))
+            # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+# Release handle to the webcam
+cv2.destroyAllWindows()
